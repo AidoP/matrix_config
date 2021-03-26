2320dca0409156e808357fa4bbc0ebcc72f60e13
AidoP <aidop@me.com>
2021-03-26 14:19:01 +0800

    Missing -c

--- postgres.sh
+++ postgres.sh
@@ -6,5 +6,5 @@ createuser --pwprompt synapse_user
 # See https://github.com/matrix-org/synapse/blob/develop/docs/postgres.md
 
 echo 'Creating the Postgres database named `synapse` owned by `synapse_user`'
-psql "CREATE DATABASE synapse ENCODING 'UTF8' LC_COLLATE='C' LC_CTYPE='C' template=template0 OWNER synapse_user;"
+psql -c "CREATE DATABASE synapse ENCODING 'UTF8' LC_COLLATE='C' LC_CTYPE='C' template=template0 OWNER synapse_user;"
 
